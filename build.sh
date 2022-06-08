997ad0785c731befc7f97a72b58a4296b89fecc5
Milan Hauth <milahu@gmail.com>
2022-06-08 11:38:48 +0200

    restore files: site/index.atom site/index.xml
    remove date tags in build.sh
    
    actions/upload-artifact fails when files are missing

--- build.sh
+++ build.sh
@@ -7,3 +7,8 @@ find . -maxdepth 1 -type l -print0 | xargs -0 rm
 
 echo creating new symlinks
 ln -s site/* ./ || true # allow to fail
+
+echo making timeless build
+# https://github.com/knadh/tg-archive/issues/68
+sed -i -E 's|<lastBuildDate>[^<]+</lastBuildDate>||g' site/index.xml
+sed -i -E 's|<updated>[^<]+</updated>||g' site/index.atom
