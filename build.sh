f9f5aea67fe7ca58191fd32320f4ca3c7b0821b4
Milan Hauth <milahu@gmail.com>
2025-12-27 09:04:56 +0100

    actually fix HTML in site_description

--- build.sh
+++ build.sh
@@ -19,7 +19,3 @@ sed -i -E 's|<updated>[^<]+</updated>||g' site/index.atom
 
 echo remove trailing whitespace
 sed -i -E 's/ +$//' site/*.{html,xml,atom}
-
-# fix: "tg-archive -b" does no longer preserve HTML in site_description
-echo patching site_description
-sed -i 's|{{{{{site_description}}}}}|<br>Telegram archive<br><div class="github-url" style="display:inline">(<a href="https://github.com/milahu/iceagefarmer">Github</a>)</div>|' site/*.{html,xml,atom}
